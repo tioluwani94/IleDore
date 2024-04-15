7f390c07cf4ac0aae31ef4bd9dec4e7a7025dd3b
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2024-04-15 21:00:49 +0100

    chore: feedback updates

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@
   let mobileMenu = document.getElementById("mobile-menu");
   let openMobileMenuBtn = document.getElementById("open-mobile-menu-btn");
   let closeMobileMenuBtn = document.getElementById("close-mobile-menu-btn");
+  let mobileNavLinks = document.querySelectorAll(".mobile-nav-link");
 
   copyRightDate.innerHTML = new Date().getFullYear();
 
@@ -11,5 +12,11 @@
   });
   closeMobileMenuBtn.addEventListener("click", () => {
     mobileMenu.classList.toggle("hidden");
+  });
+
+  mobileNavLinks.forEach((navlink) => {
+    navlink.addEventListener("click", () => {
+      mobileMenu.classList.toggle("hidden");
+    });
   });
 })();
